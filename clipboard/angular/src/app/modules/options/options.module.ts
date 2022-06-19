15f7ae402dc5c346f52143e61d33afa707b7a839
YiZhou Zhao <yizhouzhao@YiZhoudeMacBook-Pro.local>
2022-06-19 23:30:41 +0800

    update

--- clipboard/angular/src/app/modules/options/options.module.ts
+++ clipboard/angular/src/app/modules/options/options.module.ts
@@ -2,9 +2,16 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { OptionsRoutingModule } from './options-routing.module';
 import { OptionsComponent } from './pages/options/options.component';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzTreeModule } from 'ng-zorro-antd/tree';
+import { NzTreeViewModule } from 'ng-zorro-antd/tree-view';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @NgModule({
   declarations: [OptionsComponent],
-  imports: [CommonModule, OptionsRoutingModule]
+  imports: [CommonModule, OptionsRoutingModule, NzButtonModule, NzLayoutModule, NzTreeModule, NzTreeViewModule, NzIconModule]
 })
-export class OptionsModule {}
+export class OptionsModule {
+
+}
